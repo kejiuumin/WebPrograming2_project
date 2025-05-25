e3d1f918f2611d7b79624cb3b31a264bce6c153c
Leejunhyeok03 <ly0609@naver.com>
2025-05-26 05:10:29 +0900

    feat:Games, Category 추가

--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Category from "./../components/Games/Category";
 
 export default function Games() {
-  return <div>Games</div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const query = new URLSearchParams(location.search);
+  const sort = query.get("sort") || "popular";
+
+  const handleCategoryClick = (value) => {
+    navigate(`/games?sort=${value}`);
+  };
+
+  return (
+    <div className="max-w-[1200px] mx-auto mt-10">
+      <div className="w-full flex items-center justify-between gap-4">
+        <Category
+          text="인기순"
+          to="/games?sort=popular"
+          checked={sort === "popular"}
+          onClick={() => handleCategoryClick("popular")}
+        />
+        <Category
+          text="최신순"
+          to="/games?sort=latest"
+          checked={sort === "latest"}
+          onClick={() => handleCategoryClick("latest")}
+        />
+        <Category
+          text="장르별"
+          to="/games?sort=genre"
+          checked={sort === "genre"}
+          onClick={() => handleCategoryClick("genre")}
+        />
+      </div>
+    </div>
+  );
 }
